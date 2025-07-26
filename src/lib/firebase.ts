5906939ecc03e18d8ace7b43eeb3a73b7c4d79ed
Infatoz Technologies <infatoztechnologies@gmail.com>
2025-07-26 22:54:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "sahayak-ai-n9xrq",
-  appId: "1:941216237784:web:866e2ade451a0852bd2fad",
-  storageBucket: "sahayak-ai-n9xrq.firebasestorage.app",
   apiKey: "AIzaSyAqaXXc3iKSMiQjY5HxsdPsGVO3akfJ7OQ",
   authDomain: "sahayak-ai-n9xrq.firebaseapp.com",
-  messagingSenderId: "941216237784"
+  projectId: "sahayak-ai-n9xrq",
+  storageBucket: "sahayak-ai-n9xrq.firebasestorage.app",
+  messagingSenderId: "941216237784",
+  appId: "1:941216237784:web:866e2ade451a0852bd2fad"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
